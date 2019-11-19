ac4877e0271f95d969a34119eca05dd2aae740bb
epicsansman <57912371+epicsansman@users.noreply.github.com>
2019-11-19 14:26:21 -0600

    trolllll

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,1 @@
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		for (int i = 1; i <= 10; i++)
-		{
-			System.out.println(i);
-		}
-	}
-
-}
+//lmao
